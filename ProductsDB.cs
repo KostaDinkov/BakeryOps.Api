a8f843f8bb186b51886ec560f7403ec979af69a0
Kosta Dinkov <k.v.dinkov@gmail.com>
2022-11-08 09:41:40 +0200

    Add project files.

---
+++ ProductsDB.cs
@@ -0,0 +1,18 @@
+﻿namespace Orders.ProductsDB;
+
+public record Product
+{
+    public int ID { get; set; }
+    public string? Name { get; set; }
+    public decimal? Price { get; set; }
+}
+
+public class ProductsDB
+{
+    private static List<Product> products = new List<Product>()
+    {
+        new Product { ID = 1, Name = "Torta", Price = 10},
+        new Product { ID = 2, Name = "Ekler", Price = 3},
+        new Product { ID = 3, Name = "Sladki", Price = 2.5m}
+    };
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+using Microsoft.OpenApi.Models;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "Orders", Description = "Application for customer orders", Version = "v1" });
+        });
+
+        var app = builder.Build();
+
+        app.UseSwagger();
+        app.UseSwaggerUI(c =>
+        {
+            c.SwaggerEndpoint("/swagger/v1/swagger.json", "Orders API V1");
+        });
+
+
+        app.MapGet("/", () => "Hello World!");
+
+        app.Run();
+    }
+}
