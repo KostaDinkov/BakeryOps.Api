fdd86b9282f10d3f24676ff12438706c21418047
Kosta Dinkov <k.v.dinkov@gmail.com>
2024-10-18 09:43:40 +0300

    Mark GetOrdersBetween as new in IOrdersService
    The GetOrdersBetween method in the IOrdersService interface has been updated to include the new keyword. This change explicitly hides a member of the base class or interface with the same name. The return type and parameters of the method remain unchanged.

--- Services/IOrdersService.cs
+++ Services/IOrdersService.cs
@@ -8,7 +8,7 @@ namespace BakeryOps.API.Services
         Task<Order> AddOrder(OrderDTO orderDto);
         Task<Order> DeleteOrder(int id);
         Task<Order> GetOrderById(int id);
-        Task<List<List<Order>>> GetOrdersBetween(DateTime startDate, DateTime endDate);
+        new Task<List<List<Order>>> GetOrdersBetween(DateTime startDate, DateTime endDate);
         Task<IEnumerable<Order>> GetOrdersFor(DateTime startDate);
         Task<Order> UpdateOrder(int id, OrderDTO update);
     }
